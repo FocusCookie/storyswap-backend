43e738963bb0b47b88b26a303f3a6408c96f257a
Stephan Lüddemann <Stephan_Lueddemann@web.de>
2021-11-30 11:00:54 +0100

    Book route get all and get by id (#5)

--- routes/books.js
+++ routes/books.js
@@ -3,8 +3,22 @@ const router = express.Router();
 const controller = require("../controller/books");
 
 router.get("/", async (req, res) => {
-  const books = await controller.getBooks();
-  res.send(books);
+  try {
+    const books = await controller.getBooks();
+    res.send(books);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const book = await controller.getBookById(id);
+    res.send(book);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
